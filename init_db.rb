bd9afedaeb18822ba15bbcf6b550da0bdc76c468
Dan Finnie <dfinnie@noisenewyork.com>
2012-12-30 16:30:10 -0500

    Ability to read bus and rail data simulatenously.

--- init_db.rb
+++ init_db.rb
@@ -3,8 +3,16 @@
 require 'sqlite3'
 require 'csv'
 
-unless Dir.exist('bus_data') || Dir.exist('rail_data')
-	puts "Please download and extract the bus_data.zip and rail_data.zip files from the NJ Transit Developers site."
-	exit 1
+def open_bus_and_rail(filename, &blk)
+	process_csv = proc do |f|
+		c = CSV.new(f, headers: true, return_headers: false)
+		c.each(&blk)
+	end
+
+	File.open("bus_data/#{filename}.txt", "r:UTF-8", &process_csv) 
+	File.open("rail_data/#{filename}.txt", "r:UTF-8", &process_csv)
 end
 
+open_bus_and_rail("agency") do |r|
+	p r
+end
