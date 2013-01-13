39529223d8b81568b7766853dfbf3e725f0e1908
Dan Finnie <dfinnie@noisenewyork.com>
2013-01-13 16:36:24 -0500

    Add debugging statements for Database and profiling

--- lib/database.rb
+++ lib/database.rb
@@ -7,6 +7,25 @@ module NJTMap
 		def find_service_ids_for_time(t)
 			execute("select service_id from calendar_dates where date=?", t.to_s_gtfs).map(&:first)
 		end
+
+		def execute(sql, *bind_vars, &blk)
+			Log.info do
+				ret = "executing sql #{format_sql(sql).inspect}"
+				ret += " with vars #{bind_vars.inspect}" if bind_vars.length > 0
+				ret
+			end
+			super(sql, *bind_vars, &blk)
+		end
+
+		def prepare(sql)
+			Log.info { "preparing sql #{format_sql(sql).inspect}" }
+			super(sql)
+		end
+
+		private
+		def format_sql(sql)
+			sql.gsub(/\n\s*/, ' ').gsub(/\s{2,}/, ' ').strip
+		end
 	end
 
 	DB = Database.new("gtfs.db")
