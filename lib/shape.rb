0d162e533113372d42fd66a19661e55af548eea3
Dan Finnie <dfinnie@noisenewyork.com>
2013-01-18 13:10:01 -0500

    Hack to make json file work for now

--- lib/shape.rb
+++ lib/shape.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), "location.rb")
 
 module NJTMap
 	class Shape < Location
-		def initialize(shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence, shape_dist_traveled)
+		def initialize(shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence, shape_dist_traveled, *args) # *args is a quick patch until new schema is fully utilized
 			@shape_id, @shape_pt_lat, @shape_pt_lon, @shape_pt_sequence, @shape_dist_traveled = shape_id, shape_pt_lat.to_f, shape_pt_lon.to_f, shape_pt_sequence, shape_dist_traveled
 		end
 
