d3692fbae8da8ac2818d46bbe74af45e5ad01e29
Dan Finnie <dfinnie@noisenewyork.com>
2013-01-27 17:56:07 -0500

    Change indentation

--- web/script.js
+++ web/script.js
@@ -1,34 +1,34 @@
 function initialize() {
-var mapOptions = {
-  center: new google.maps.LatLng(40.171877413034224, -74.71853198262102),
-  zoom: 10,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+  var mapOptions = {
+    center: new google.maps.LatLng(40.171877413034224, -74.71853198262102),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-var currentMarkers = [];
-// var nextMarkers = {}
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  var currentMarkers = [];
+  // var nextMarkers = {}
 
-dataGatherer = window.setInterval(function() {
-  var cacheBuster = Math.floor(Math.random()*10000) + "";
-  $.get("data.json", cacheBuster, function(data, textStatus, jqXhr) {
-    nextMarkers = [];
-    $.each(data.locs, function() {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(this.lat, this.lon),
-        title: this.trip + " btwn " + this.from + " and " + this.to
+  dataGatherer = window.setInterval(function() {
+    var cacheBuster = Math.floor(Math.random()*10000) + "";
+    $.get("data.json", cacheBuster, function(data, textStatus, jqXhr) {
+      nextMarkers = [];
+      $.each(data.locs, function() {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(this.lat, this.lon),
+          title: this.trip + " btwn " + this.from + " and " + this.to
+        });
+        marker.setMap(map);
+        nextMarkers.push(marker);
       });
-      marker.setMap(map);
-      nextMarkers.push(marker);
-    });
 
-    // Garbage collect old markers and remove them from the map
-    // (they are no longer in existence)
-    $.each(currentMarkers, function() {
-      this.setMap(null);
-    });
-    
-    currentMarkers = nextMarkers;
-  }, "json");
-}, 1000);
+      // Garbage collect old markers and remove them from the map
+      // (they are no longer in existence)
+      $.each(currentMarkers, function() {
+        this.setMap(null);
+      });
+
+      currentMarkers = nextMarkers;
+    }, "json");
+  }, 1000);
 }
