d6ff2f15b513d3ea3d87ba33558cccd684ad2089
Dan Finnie <dfinnie@noisenewyork.com>
2013-01-27 15:36:40 -0500

    Move scripts and styles to separate files

---
+++ web/script.js
@@ -0,0 +1,44 @@
+function initialize() {
+var mapOptions = {
+  center: new google.maps.LatLng(40.171877413034224, -74.71853198262102),
+  zoom: 10,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+var currentMarkers = {};
+// var nextMarkers = {}
+
+dataGatherer = window.setInterval(function() {
+  var cacheBuster = Math.floor(Math.random()*10000) + "";
+  $.get("data.json", cacheBuster, function(data, textStatus, jqXhr) {
+    nextMarkers = {}
+    $.each(data.locs, function() {
+      var existingMarker = currentMarkers[this.trip_id];
+      var marker;
+      delete currentMarkers[this.trip_id];
+
+      if (existingMarker) {
+        existingMarker.setPosition(new google.maps.LatLng(this.lat, this.lon));
+        existingMarker.setTitle(this.trip + " btwn " + this.from + " and " + this.to);
+        marker = existingMarker;
+      } else {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(this.lat, this.lon),
+          title: this.trip + " btwn " + this.from + " and " + this.to
+        });
+        marker.setMap(map);
+      }
+      
+      nextMarkers[this.trip_id] = marker;
+    });
+
+    // Garbage collect old markers and remove them from the map
+    // (they are no longer in existence)
+    $.each(currentMarkers, function() {
+      this.setMap(null);
+    });
+    currentMarkers = nextMarkers;
+  }, "json");
+}, 1000);
+}
