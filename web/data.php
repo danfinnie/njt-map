11865a2b72a4961f24ea6ce738b1b00f227b15f4
Dan Finnie <dfinnie@noisenewyork.com>
2013-01-28 20:58:27 -0500

    PHP port produces valid but wrong JSON

--- web/data.php
+++ web/data.php
@@ -30,8 +30,31 @@ $stmt->bindValue('time', $seconds_into_day, PDO::PARAM_INT);
 $stmt->execute();
 
 echo "{\"locs\": [";
-$delim = ' ';
+$delim = '';
 
 while($row = $stmt->fetch(PDO::FETCH_NUM)) {
-	print_r($row);
+	// JSON outputting
+	echo $delim;
+	$delim = ',';
+
+	$trip_id = $row[0];
+    $shape_id = $row[1];
+    $first_dept_time = $row[2];
+    $second_dept_time = $row[3];
+    $first_dist_traveled = $row[4];
+    $second_dist_traveled = $row[5];
+
+    $fraction_complete = ($seconds_into_day - $first_dept_time) * 1.0 / ($second_dept_time - $first_dept_time);
+    $dist_traveled = $fraction_complete * ($second_dist_traveled - $first_dist_traveled) + $first_dist_traveled;
+
+    //$pos = calc_lat_lon($shape_id, $dist_traveled);
+    $pos = array("lat" => 42, "lon" => 45.3);
+
+    echo json_encode(array(
+    	"trip_id" => $trip_id,
+    	"lat" => $pos['lat'],
+    	"lon" => $pos['lon'],
+    ));
 }
+
+echo ']}';
