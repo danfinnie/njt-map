b5dc20c17a1502ad6def77a1ac5e6e012913e3a7
Dan Finnie <dfinnie@noisenewyork.com>
2013-01-29 19:50:54 -0500

    Fix indentation, separate commit to preserve the diff

--- web/script.js
+++ web/script.js
@@ -1,54 +1,54 @@
 initialize = (function() { 
-var map;
+  var map;
 
-$(function() {
-  $("#collapse").on("click", function() {
-    $("body").toggleClass("collapsed");
-    window.setTimeout(function() {
-      google.maps.event.trigger(map, "resize");
-    }, 1000);
+  $(function() {
+    $("#collapse").on("click", function() {
+      $("body").toggleClass("collapsed");
+      window.setTimeout(function() {
+        google.maps.event.trigger(map, "resize");
+      }, 1000);
+    });
   });
-});
 
-return (function() {
-  var mapOptions = {
-    center: new google.maps.LatLng(40.171877413034224, -74.71853198262102),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+  return (function() {
+    var mapOptions = {
+      center: new google.maps.LatLng(40.171877413034224, -74.71853198262102),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  window.map = map;
-  var currentMarkers = [];
-  // var nextMarkers = {}
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    window.map = map;
+    var currentMarkers = [];
+    // var nextMarkers = {}
 
-  dataGatherer = function() {
-    $("body").addClass("loading");
-    var cacheBuster = Math.floor(Math.random()*10000) + "";
-    $.get("data.php", cacheBuster, function(data, textStatus, jqXhr) {
-      nextMarkers = [];
-      $.each(data.locs, function() {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(this.lat, this.lon),
-          title: "Unknown trip" // this.trip + " btwn " + this.from + " and " + this.to
+    dataGatherer = function() {
+      $("body").addClass("loading");
+      var cacheBuster = Math.floor(Math.random()*10000) + "";
+      $.get("data.php", cacheBuster, function(data, textStatus, jqXhr) {
+        nextMarkers = [];
+        $.each(data.locs, function() {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(this.lat, this.lon),
+            title: "Unknown trip" // this.trip + " btwn " + this.from + " and " + this.to
+          });
+          marker.setMap(map);
+          nextMarkers.push(marker);
         });
-        marker.setMap(map);
-        nextMarkers.push(marker);
-      });
 
-      // Garbage collect old markers and remove them from the map
-      // (they are no longer in existence)
-      $.each(currentMarkers, function() {
-        this.setMap(null);
-      });
+        // Garbage collect old markers and remove them from the map
+        // (they are no longer in existence)
+        $.each(currentMarkers, function() {
+          this.setMap(null);
+        });
 
-      currentMarkers = nextMarkers;
+        currentMarkers = nextMarkers;
 
-      // Queue up next iteration only after all processing has been done to avoid choking bandwidth/CPU.
-      $("body").removeClass("loading");
-      window.setTimeout(dataGatherer, 1000);
-    }, "json");
-  };
-  dataGatherer();
-});
+        // Queue up next iteration only after all processing has been done to avoid choking bandwidth/CPU.
+        $("body").removeClass("loading");
+        window.setTimeout(dataGatherer, 1000);
+      }, "json");
+    };
+    dataGatherer();
+  });
 })();
